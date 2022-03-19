82143a7dfeffa83570b1c5868e906d51a66e63b4
EdinarWeb <edinarweb@gmail.com>
2022-03-19 09:36:56 -0300

    Resolvi algunas tareas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -121,14 +121,22 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function elevarAlCuadrado(num) {
