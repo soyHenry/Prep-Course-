97aab93a12a1851bcaa147f064d024cb895f364a
EdinarWeb <edinarweb@gmail.com>
2022-03-20 22:02:06 -0300

    resalice once tareas

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -159,6 +159,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero === 0 || numero === 1) {
+    return false;
+  }else {
+    for(let i = 2; i < numero; i++) {
+      if(numero % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 function esVerdadero(valor){
