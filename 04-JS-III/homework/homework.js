6f405b08b2d2f122cab5eda5931f8ce23e8dbb6f
Carlos Carmona <carloscarmona0829@gmail.com>
2022-04-27 10:57:47 -0500

    homework and explanations completed

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -153,35 +153,81 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let str = n.toString();
+  if (str.charAt(0) === "9") {
+    return true;
+  }
+  return false;
 }
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
+  let data=arreglo[0];
+  for (let i = 0; i < arreglo.length; i++) {    
+    if (arreglo[i] !== data) {
+      return false;
+      }
+  }
+  return true;
 }
 
 function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
+  // Tu código:  
+  newArray=[];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+      newArray.push(array[i]);
+    }    
+  }
+  if (newArray.length < 3){
+    return "No se encontraron los meses pedidos";
+  }
+  return newArray;
 }
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let mayores=[];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]>100){
+      mayores.push(array[i]);
+    }
+  }
+  return mayores;
 }
 
 function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.
   //Devolver el array
-  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y
+  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, 
+  //debe interrumpirse la ejecución y
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  //Este ejercicio no lo pude resolver, no entendí el enunciado y puse la respuesta dada por Henry
+  var array = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++) {
+    suma = suma + 2;
+    if (suma === i) break;
+    else {
+      array.push(suma);
+    }
+  }
+  if (i < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+    return array;
+  }    
 }
 
 function continueStatement(numero) {
@@ -191,6 +237,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++) {
+    if (i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }    
+  }
+  return array;
 }
 
 // No modificar nada debajo de esta línea
